005036a9052d83870eab43c9c3eb6939357d41fa
Eric J <34559118+jiapurdue@users.noreply.github.com>
2018-07-01 18:33:08 -0400

    add footer

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,8 +4,8 @@ import { Col } from "../../components/Grid";
 import './Card.css'
 
 const Card = props => (
-        <Col size="md-4">
-            <img className = 'cardImg rounded' src={props.url} alt={props.id} key={props.id} name={props.name}/>
+        <Col size="md-3">
+            <img className = 'cardImg rounded shadow-lg' src={props.url} alt={props.id} key={props.id} name={props.name}/>
         </Col>
 );
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,6 +3,7 @@ import Jumbotron from "../../components/Jumbotron";
 import Nav from "../../components/Nav";
 import { Row, Container } from "../../components/Grid";
 import Card from "../../components/Card";
+import Footer from "../../components/Footer";
 import Dog from "../../Dog.json";
  
 import './Main.css'
@@ -43,11 +44,15 @@ class Main extends Component {
                 <Jumbotron title = {'Clicky Game'} 
                 subLine = {`Click on an image to earn points, but don't click on any more than once!`} />
 
+                <Container>
                 <Row>
                 {this.state.dogs.map(dog => {
                     return (<Card key = {dog.id} id = {dog.id} url = {dog.url} name ={dog.name}/> )
                 })}
                 </Row>
+                </Container>
+
+                <Footer />
 
             </Container>
         )
